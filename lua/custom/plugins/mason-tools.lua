6ae65644bdfbee8ab34a21b93ff89d5f034e4a45
Jack <jack.jr@me.com>
2025-06-13 22:09:42 -0700

    changed config

--- lua/custom/plugins/mason-tools.lua
+++ lua/custom/plugins/mason-tools.lua
@@ -1,11 +1,49 @@
 return {
-  'williamboman/mason-lspconfig.nvim',
-  opts = {
-    ensure_installed = {
-      'pyright',
-      'lua_ls',
-      'bashls',
-      'jsonls',
-    },
+  -- Extend Mason to install non-LSP tools
+  {
+    'williamboman/mason.nvim',
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, {
+        'css-lsp',
+        'eslint-lsp',
+        'flake8',
+        'html-lsp',
+        'lua-language-server',
+        'pyright',
+        'stylua',
+        'typescript-language-server',
+      })
+    end,
+  },
+
+  -- Extend Mason-lspconfig to install LSP servers
+  {
+    'williamboman/mason-lspconfig.nvim',
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, {
+        'cssls',
+        'eslint',
+        'html',
+        'lua_ls',
+        'pyright',
+        'tsserver',
+      })
+    end,
+  },
+
+  -- Optional: make sure lspconfig knows to configure these servers
+  {
+    'neovim/nvim-lspconfig',
+    opts = function(_, opts)
+      opts.servers = opts.servers or {}
+      opts.servers.cssls = opts.servers.cssls or {}
+      opts.servers.eslint = opts.servers.eslint or {}
+      opts.servers.html = opts.servers.html or {}
+      opts.servers.lua_ls = opts.servers.lua_ls or {}
+      opts.servers.pyright = opts.servers.pyright or {}
+      opts.servers.tsserver = opts.servers.tsserver or {}
+    end,
   },
 }
